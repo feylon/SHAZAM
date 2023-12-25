b8b73049e53bf783b091a2c0bdd578967764c0fc
feylon <jamshid14092002@gmail.com>
2023-12-25 14:46:08 +0500

    Ish boshlandi

--- src/Home.vue
+++ src/Home.vue
@@ -1,3 +1,19 @@
-<template></template>
+<template>
+    <nav>
+        <h3>
+            Shazam
+        </h3>
+        <div class="right_nav">
+            <p>
+                Pricing
+            </p>
+            <button class="join_discord"> Join Discord</button>
+            <button class="login">Login</button>
+        </div>
+    </nav>
+</template>
 <script></script>
-<style></style>
+<style lang="scss">
+@import "home.scss";
+
+</style>
