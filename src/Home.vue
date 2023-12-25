a352112d1d7f9cd5ec2b3f7d54b1ac94559c9e5b
feylon <jamshid14092002@gmail.com>
2023-12-25 15:01:36 +0500

    15:01 25 december

--- src/Home.vue
+++ src/Home.vue
@@ -1,16 +1,18 @@
 <template>
-    <nav>
+  <div class="box">
+    <nav class="home_nav">
         <h3>
             Shazam
         </h3>
         <div class="right_nav">
-            <p>
+            <p class="pricing">
                 Pricing
             </p>
             <button class="join_discord"> Join Discord</button>
             <button class="login">Login</button>
         </div>
     </nav>
+  </div>
 </template>
 <script></script>
 <style lang="scss">
